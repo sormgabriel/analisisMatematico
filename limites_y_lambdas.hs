25f653b6ff87b512903c9e401c5c1a766c7cc005
marichan <marichan@marichan.marichan>
2017-08-12 13:35:29 -0300

    Se agregan otras funciones para imiprimir salidas de los resultados de las funciones

--- limites_y_lambdas.hs
+++ limites_y_lambdas.hs
@@ -4,8 +4,30 @@ ejercicio9::[Float]->[(Float,Float)]
 ejercicio9 floatList  = let func = (\x-> ((x - 1.0)/((x ** 3)-1.0),x))
 	     		in (map func floatList)
 
+ejercicio10::[Float]->[(Float,Float)]
+ejercicio10 floatList = let func = (\x-> ((((raizN 3 x )-1)/((raizN 2 x)-1)),x))
+			in (map func floatList)
+
+
+ejercicio11::[Float]->[(Float,Float)]
+ejercicio11 floatList = map (\x->((1 - cos x)/x**2,x)) floatList
+ 
+ejercicio12::[Float]->[(Float,Float)]
+ejercicio12 floatList = map (\x-> (funcion12 x,x)) floatList
+
+funcion12::Float->Float
+funcion12 f = (raizN 2 f) * (log f)
+
+raizN::Float->Float->Float
+raizN n f = f ** (1/n)
+
+imprimirResultadoEn::String->([Float] ->[(Float,Float)])-> [Float] -> IO()
+imprimirResultadoEn path function parameter = writeFile path $ unlines.map show $ function parameter
+
 
 tiendeAUno::[Float]
-tiendeAUno = [0.2, 0.4, 0.6, 0.8, 0.9, 0.99, 1.01, 1.1, 1.2, 1.4, 1.6, 1.8] 
+tiendeAUno = [0.2, 0.4, 0.6, 0.8, 0.9, 0.99, 1.001, 1.01, 1.1, 1.2, 1.4,1.5, 1.6, 1.8] 
 
+tiendeACero::[Float]
+tiendeACero = [1, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01, 0.005,  0.001, 0.0001, 0.00001]
 
